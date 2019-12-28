a69fd86681469856cab4bcdb397abefbd738fa8d
Liora Milbaum <liora@lmb.co.il>
2019-12-28 15:15:13 +0200

    subscribe form

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+import request
+from flask import Flask, flash, redirect, render_template, request, url_for
+from wtforms import Form, PasswordField, StringField, validators
 
 app = Flask(__name__)
 
@@ -8,5 +10,45 @@ app = Flask(__name__)
 def hello():
     return 'Hello World!'
 
+
+class SubscribeForm(Form):
+    name = StringField('Name', [
+        validators.Regexp(r'[A-Za-z\s]+',
+                          message="Name may only contain alphanumeric \
+                          characters and spaces"),
+        validators.Length(min=1, max=50)
+    ])
+    email = StringField('Email', [
+        validators.Email(),
+        validators.Length(min=6, max=50)
+    ])
+    username = StringField('Username', [
+        validators.Regexp(r'[A-Za-z0-9_]+',
+                          message="Name may only contain alphanumeric \
+                          characters"),
+        validators.Length(min=4, max=25)
+    ])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords do not match'),
+
+    ])
+    confirm = PasswordField('Confirm Password')
+
+
+@app.route('/subscribe', methods=['GET', 'POST'])
+def subscribe():
+    form = SubscribeForm(request.form)
+
+    if request.method == 'POST' and form.validate():
+        name = form.name.data
+        email = form.email.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+
+    return render_template('subscribe.html', form=form)
+
+
 if __name__ == '__main__':
+    app.secret_key = 'secret123'
     app.run(debug=True, host='0.0.0.0')
