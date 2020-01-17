f9d3beb86de8e2f53376298c7cad72bc390655a3
Liora Milbaum <liora@lmb.co.il>
2020-01-17 09:33:21 +0200

    test logo on all pages

--- tests/functional/test_pages.py
+++ tests/functional/test_pages.py
@@ -14,3 +14,4 @@ def test_page(test_client, page):
     response = test_client.get(page)
     assert response.status_code == 200
     assert b'<title>Zapata</title>' in response.data
+    assert b'<h2>Zapata</h2>' in response.data
