af403c6b087c63fb3066cf1d077543acaa035904
Liora Milbaum <liora@lmb.co.il>
2020-01-17 08:53:28 +0200

    fix broken navbar

--- zapata/routes.py
+++ zapata/routes.py
@@ -6,5 +6,18 @@ from flask import render_template
 def configure_routes(app):
 
     @app.route('/')
-    def index():
+    @app.route('/home')
+    def home():
         return render_template('index.html')
+
+    @app.route('/about')
+    def about():
+        return render_template('about.html')
+
+    @app.route('/services')
+    def services():
+        return render_template('services.html')
+
+    @app.route('/contact')
+    def contact():
+        return render_template('contact.html')
