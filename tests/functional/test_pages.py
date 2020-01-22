0f0033b7c7bee43478ddcd386073f1331dd68e39
Liora Milbaum <liora@lmb.co.il>
2020-01-22 07:04:55 +0200

    Update the banner

--- tests/functional/test_pages.py
+++ tests/functional/test_pages.py
@@ -6,9 +6,9 @@ import pytest
 @pytest.mark.parametrize('page', [
     '/',
     '/home',
-    'contact',
-    'about',
-    'services'
+    '/contact',
+    '/about',
+    '/services'
 ])
 def test_page(test_client, page):
     response = test_client.get(page)
