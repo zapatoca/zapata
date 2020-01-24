397bdd26285fa5fb2a70bc765e9c64d5530553c1
Liora Milbaum <liora@lmb.co.il>
2020-01-24 22:50:04 +0200

    refactoring pytest

--- tests/functional/test_ui.py
+++ tests/functional/test_ui.py
@@ -7,6 +7,12 @@ from flask import url_for
 @pytest.mark.usefixtures('live_server')
 class TestLiveServer():
 
-    def test_server_is_up_and_running(self, selenium):
-        selenium.get(url_for('home', _external=True))
+    @pytest.mark.parametrize('page', [
+        'home',
+        'contact',
+        'about',
+        'services'
+    ])
+    def test_server_is_up_and_running(self, selenium, page):
+        selenium.get(url_for(page, _external=True))
         assert selenium.title == 'Zapata'
