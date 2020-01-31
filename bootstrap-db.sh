d0fc621df62537b950760bdd8105bd9aadfbecbe
Liora Milbaum <liora@lmb.co.il>
2020-01-31 17:58:00 +0200

    Create a subscribers table #52

--- bootstrap-db.sh
+++ bootstrap-db.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
+debconf-set-selections <<< 'mysql-server mysql-server/root_password password 12345'
+debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password 12345'
 
 apt-get update
 apt-get install -y mysql-server
 
 systemctl enable mysql
 systemctl start mysql
+
+mysql -u root -p12345 -e "CREATE DATABASE zapata;"
+mysql -u root -p12345 zapata < /vagrant/create_db.sql
