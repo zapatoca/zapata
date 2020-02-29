923bcc7b1c25748ac8cf93fc76f9e9b620e53ce5
Liora Milbaum <liora@lmb.co.il>
2020-02-29 17:51:11 +0200

    jumbotron

--- tests/functional/test_ui.py
+++ tests/functional/test_ui.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import time
+
 import pytest
 from flask import url_for
 
@@ -21,6 +23,26 @@ class TestLiveServer():
         selenium.get(url_for(page, _external=True))
         selenium.find_element_by_class_name('navbar-brand').click()
         assert selenium.current_url == url_for(page, _external=True)
+
+    @pytest.mark.parametrize('page', [
+        'home'
+    ])
+    def test_header(self, selenium, page):
+        selenium.get(url_for(page, _external=True))
+        selenium.execute_script(
+            "window.scrollTo(0,document.body.scrollHeight)")
+        time.sleep(3)
+        element1 = selenium.find_element_by_class_name("background-header")
+        element2 = selenium.find_element_by_class_name(
+            "navbar-brand").find_element_by_tag_name("h2")
+        element3 = selenium.find_element_by_class_name(
+            "navbar-brand").find_element_by_tag_name("h3")
+        assert element1.value_of_css_property(
+            "background-color") == "rgb(255, 255, 255)"
+        assert element2.value_of_css_property(
+            "color") == "rgb(30, 30, 30)"
+        assert element3.value_of_css_property(
+            "color") == "rgb(30, 30, 30)"
 
     @pytest.mark.parametrize('test_input', [
         ('kuku@stam.com')
