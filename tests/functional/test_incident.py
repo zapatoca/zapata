2c0f4032aee89bfe105e30180836950bf141e3d9
Liora Milbaum <liora@lmb.co.il>
2021-03-05 15:15:20 +0200

    certbot

--- tests/functional/test_incident.py
+++ tests/functional/test_incident.py
@@ -1,7 +1,7 @@
 def test_file_incident(selenium):
-    selenium.get("http://localhost:5000/incident")
+    selenium.get("https://localhost:8443/incident")
     selenium.find_element_by_id("description").send_keys(
         "reporting an incident"
     )
     selenium.find_element_by_id("submit").click()
-    assert "http://localhost:5000/" == selenium.current_url
+    assert "https://localhost:8443/" == selenium.current_url
--- tests/functional/test_projects.py
+++ tests/functional/test_projects.py
@@ -1,5 +1,5 @@
 def test_new_project(selenium):
-    selenium.get("http://localhost:5000/projects/new")
+    selenium.get("https://localhost:8443/projects/new")
     assert (
         selenium.find_element_by_class_name("card-header").text
         == "New Project"
